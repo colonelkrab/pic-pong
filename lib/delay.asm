69edbdfb5b0a594c1603d904c9d2434e31fe8067
Alfin <alfinbaiju@proton.me>
2025-10-20 11:44:42 +0530

    code to turn on display

---
+++ lib/delay.asm
@@ -0,0 +1,33 @@
+#include <p18f452.inc>
+global _delay_125ms
+
+.DELAY_DATA udata
+d0 res 1
+d1 res 1
+d2 res 1 
+
+.DELAY code
+; Delay = 0.125 seconds
+; Clock frequency = 24 MHz
+_delay_125ms:
+movlw d'100'
+movwf d0
+i:
+movlw d'100'
+movwf d1
+j:
+movlw d'24'
+movwf d2
+k: 
+decfsz d2,f
+goto k
+
+decfsz d1,f
+goto j
+
+decfsz d0,f
+goto i
+
+return
+end
+
--- main.asm
+++ main.asm
@@ -1,16 +1,18 @@
 #include <p18f452.inc>
+#define F_CPU 24000000
 #include <include/uart.inc>
-#include <include/spi.inc>
-#define F_CPU 16000000
+#include <include/st7735.inc>
+
 config OSC=HS
 config WDT=OFF
+config LVP=ON
 
 .RESET code 0x00
 goto _main
 
 .MAIN code
 _main:
+__st7735_spi_init
+__st7735_init_sequence
 goto $
 end
-
-
